71ee613f9e40147566bbfe24c50be1eb07647402
detbin <116663162+detbin@users.noreply.github.com>
2022-10-30 12:16:44 +0100

    Add files via upload

---
+++ project7/src/com/examples/project7.java
@@ -0,0 +1,24 @@
+package com.examples;
+
+public class project7 {
+    public static void main(String[] args) {
+        int variable1 = 0;
+        boolean variable2 = true;
+        String variable3 = "prueba";
+        float variable4 = (float) 4.9;
+        double variable5 = 4.95;
+        char variable6 = 'a';
+        long variable7 = 100;
+        byte variable8 = 1;
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+        System.out.println(variable5);
+        System.out.println(variable6);
+        System.out.println(variable7);
+        System.out.println(variable8);
+    }
+}
+
+
